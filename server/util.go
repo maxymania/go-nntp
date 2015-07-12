30b5762a6858d5d89bd4ebdfb2bb0815aaf4a2e8
Simon Schmidt <simon.schmidt@htp-tel.de>
2015-07-12 09:55:18 +0200

    util: adding Helper functions

--- server/util.go
+++ server/util.go
@@ -1,6 +1,8 @@
 package nntpserver
 
 import "strconv"
+import "strings"
+import "net/textproto"
 
 func articleIDOrNumber(id string) (int64,bool) {
 	for len(id)>1 && id[0]=='0' {
@@ -8,4 +10,34 @@ func articleIDOrNumber(id string) (int64,bool) {
 	}
 	i,e := strconv.ParseInt(id,10,64)
 	return i,e==nil
+}
+
+func ArticleIDOrNumber(id string) (int64,bool) {
+	return articleIDOrNumber(id)
+}
+
+func Downlimit(a,b int64) int64{
+	if a<b { return b }
+	return a
+}
+
+func Uplimit(a,b int64) int64{
+	if a>b { return b }
+	return a
+}
+
+func splitgroups(grps string) []string{
+	if grps=="" { return []string{} }
+	return strings.Split(grps,",")
+}
+
+func GetGroups(t textproto.MIMEHeader) (r []string){
+	gg,_ := t["Newsgroups"] // nil if not exist
+	for _,g := range gg {
+		r = append(r,splitgroups(g)...)
+	}
+	for i,s := range r {
+		r[i] = strings.Trim(s," \t\r\n")
+	}
+	return r
 }
